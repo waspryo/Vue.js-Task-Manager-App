6d2f58f225b0cdc8bb9b3857e28831f1373b64d7
waspryo <wasp2123321@gmail.com>
2019-12-30 17:47:28 +0900

    Adding More Styling and Computed Properties

--- app.js
+++ app.js
@@ -4,29 +4,37 @@ var app = new Vue({
     tasks: [
       {
         id: 1,
-        name: 'Todo 1',
-        description: 'This is a todo',
+        name: "Todo 1",
+        description: "This is a todo",
         completed: false
       },
       {
         id: 2,
-        name: 'Todo 2',
-        description: 'This is another todo',
+        name: "Todo 2",
+        description: "This is another todo",
         completed: false
       },
       {
         id: 3,
-        name: 'three',
-        description: 'This is a complete todo',
+        name: "three",
+        description: "This is a complete todo",
         completed: true
       },
       {
         id: 4,
-        name: 'Four',
-        description: 'This is another complete todo',
+        name: "Four",
+        description: "This is another complete todo",
         completed: true
-      },
+      }
     ],
     message: "Hello underworld"
+  },
+  computed: {
+    completedTasks: function() {
+      return this.tasks.filter(item => item.completed == true);
+    },
+    todoTasks: function() {
+      return this.tasks.filter(item => item.completed == false);
+    }
   }
 });
