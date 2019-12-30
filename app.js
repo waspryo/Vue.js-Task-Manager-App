aa5897cca35e80b8f4912e69b95d058705ae2511
waspryo <wasp2123321@gmail.com>
2019-12-30 17:38:48 +0900

    Adding and Displaying Task List

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello underworld'
-    },
-})
+  el: "#app",
+  data: {
+    tasks: [
+      {
+        id: 1,
+        name: 'Todo 1',
+        description: 'This is a todo',
+        completed: false
+      },
+      {
+        id: 2,
+        name: 'Todo 2',
+        description: 'This is another todo',
+        completed: false
+      },
+      {
+        id: 3,
+        name: 'three',
+        description: 'This is a complete todo',
+        completed: true
+      },
+      {
+        id: 4,
+        name: 'Four',
+        description: 'This is another complete todo',
+        completed: true
+      },
+    ],
+    message: "Hello underworld"
+  }
+});
